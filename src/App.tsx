c284e79393760f1ed9092a28a1f2e3281c751e53
progjaoo <joaomvprog@gmail.com>
2026-03-13 11:21:52 -0300

    feat: push for ti group

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const Layout = () => {
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="white" enableSystem>
       <TooltipProvider>
         <Toaster />
         <Sonner />
